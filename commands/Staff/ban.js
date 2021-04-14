1d323b8c4c130f92c5bf63d0daed6527997eeb19
Dan <scarperino@gmail.com>
2021-04-14 19:36:39 +0300

    Ban Command

--- commands/Staff/ban.js
+++ commands/Staff/ban.js
@@ -1,33 +1,68 @@
-const Discord = require("discord.js");
-
-const NO_APPEAL_MSG = "You are not allowed to appeal this ban."
-const APPEAL_MSG = "You are allowed to appeal this ban. Appeal at:\nhttps://discord.gg/dxgUeYS3KK"
-const NOTICE_MSG = "You have been banned from the Script-Ware server.";
+const noAppealMsg = 'You are not allowed to appeal this ban.'
+const appealMsg = 'You are allowed to appeal this ban. Appeal At:\nhttps://discord.gg/dxgUeYS3KK'
+const noticeMsg = 'You have been banned from the [name] server.'
 
 const Command = {
-    func: (sender, mentions, channel, args) => {
-        if (mentions.length < 1)
-            return; 
+  alias: [],
+  description: 'Ban command',
+  func: async ({ Client, member, mentions, message, channel }) => {
+    if (mentions.length < 1) return
 
-        const CURRENT_DATE = new Date();
-        const SENDER_ID = sender.id;
+    const target = mentions[0]
 
-        const argsLength = args.length;
-        const noAppeal = args[argsLength - 1].toLowerCase() == "(na)" && args.pop();
-        const banMessage = args.join(" ");
+    const currentDate = new Date()
+    const senderId = member.id
 
-        const BAN_EMBED = new Discord.MessageEmbed()
-        .setAuthor("Script-Ware Bot")
-        .setImage(sender.user.avatar)
-        .addField("Notice:", NOTICE_MSG)
-        .addField("Banned By:", `<@${SENDER_ID}> with an ID: ${SENDER_ID}`)
-        .addField("Banned At:", CURRENT_DATE.toUTCString())
-        .addField("Ban Reason:", banMessage)
-        .addField("Appeal", noAppeal && NO_APPEAL_MSG || APPEAL_MSG)
-        .setTimestamp(CURRENT_DATE);
+    const argsLength = message.length
+    const noAppeal = message[argsLength - 1].toLowerCase() === '(na)' && message.pop()
 
-        
+    message.shift() // Remove the target mention
+    if (message.length < 1) message = ['No reason specified.']
+
+    const banMessage = message.join(' ')
+
+    /* eslint-disable camelcase */
+    const banEmbed = {
+      content: '',
+      embed: {
+        color: 8238467,
+        timestamp: currentDate.toString(),
+
+        author: {
+          name: 'Moderation Action',
+          icon_url: 'https://cdn.discordapp.com/attachments/778562386063917086/831646271462834216/shut_up_dino.png'
+        },
+
+        fields: [
+          {
+            name: 'Notice:',
+            value: noticeMsg.replace('[name]', channel.guild.name)
+          },
+          {
+            name: 'Banned By:',
+            value: `<@${senderId}> (${senderId})`
+          },
+          {
+            name: 'Banned At:',
+            value: currentDate.toUTCString()
+          },
+          {
+            name: 'Ban Reason:',
+            value: banMessage
+          },
+          {
+            name: 'Appeal:',
+            value: (noAppeal && noAppealMsg) || appealMsg
+          }
+        ]
+      }
     }
+    /* eslint-enable camelcase */
+
+    if (target.bannable) await target.ban(`${banMessage}, by ${member.user.tag}`).catch(() => {})
+    Client.giveFeedback(channel, 'ban', 'Well... There they go ~', 'https://cdn.discordapp.com/emojis/777791373130203136.png?v=1', 'Banned')
+    target.send(banEmbed).catch(() => {})
+  }
 }
 
-module.exports = Command;
+module.exports = Command
